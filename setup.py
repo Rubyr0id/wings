bf286cbb8cf10af9d7941568d427a29eeec4982f
Rubyr0id <adubrovitskiy@gmail.com>
2018-09-09 15:24:06 +0300

    install requirements
    'itertools','datetime','sklearn'

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     # Needed to actually package something
     packages=['wings'],
     # Needed for dependencies
-    install_requires=['numpy','pandas',''],
+    install_requires=['numpy','pandas','itertools','datetime','sklearn'],
     # *strongly* suggested for sharing
     version='1.1',
     # The license can be anything you like
